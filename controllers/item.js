de40096b9029a457479365669cb25c8ac0a2507f
Jeanette Fairless & Jocelyn Alsdorf <student@epicodus.com>
2015-06-11 15:06:53 -0700

    image editing now works

--- controllers/item.js
+++ controllers/item.js
@@ -5,39 +5,45 @@ Auction.ItemController = Ember.Controller.extend({
     edit: function() {
       this.set('isEditing', true);
     },
+
     save: function() {
       this.set('isEditing', false);
-
       this.get('model').save();
-
       this.transitionToRoute('items');
     },
+
     delete: function(){
       if(confirm("are you sure?")) {
         this.get('model').destroyRecord();
         this.transitionToRoute('items');
       }
     },
+
     showFullsize: function() {
       $(".picture-group").addClass('hidden');
       $(".fake").removeClass('hidden');
     },
+
     hideFullsize: function() {
       $(".picture-group").removeClass('hidden');
       $(".fake").addClass('hidden');
     },
+
     showFullsize2: function() {
       $(".picture-group").addClass('hidden');
       $(".fake2").removeClass('hidden');
     },
+
     hideFullsize2: function() {
       $(".picture-group").removeClass('hidden');
       $(".fake2").addClass('hidden');
     },
+
     showFullsize3: function() {
       $(".picture-group").addClass('hidden');
       $(".fake3").removeClass('hidden');
     },
+
     hideFullsize3: function() {
       $(".picture-group").removeClass('hidden');
       $(".fake3").addClass('hidden');
@@ -47,6 +53,7 @@ Auction.ItemController = Ember.Controller.extend({
       $(".picture-group").addClass('hidden');
       $(".fake4").removeClass('hidden');
     },
+
     hideFullsize4: function() {
       $(".picture-group").removeClass('hidden');
       $(".fake4").addClass('hidden');
